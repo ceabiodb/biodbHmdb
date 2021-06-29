c13f6e3f4e18975dc9d9a1258ce95447e1f3ab25
Pierrick Roger <pk.roger@icloud.com>
2021-06-29 10:07:21 +0200

    Switch to R6. (#18)

--- tests/testthat/zip_builder.R
+++ tests/testthat/zip_builder.R
@@ -5,12 +5,13 @@ if ( ! dir.exists(outdir))
 
 # Build HMDB zip
 ext_data <- system.file("extdata", package="biodbHmdb")
+test_ref <- system.file("testref", package="biodbHmdb")
 two_entries_zip_file <- file.path(ext_data,'generated', "hmdb_extract.zip")
 if ( ! file.exists(two_entries_zip_file)) {
     folder <- dirname(two_entries_zip_file)
     if ( ! dir.exists(folder))
         dir.create(folder, recursive=TRUE)
-    xmlfile <- file.path(getwd(), 'res', 'hmdb_two_full_entries.xml')
+    xmlfile <- file.path(test_ref, 'hmdb_two_full_entries.xml')
     testthat::expect_true(file.exists(xmlfile))
     utils::zip(two_entries_zip_file, xmlfile, flags='-jq')
 }
